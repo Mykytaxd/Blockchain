98848f66e67a136b6555503f9b08cac9afb83b24
Mykytaxd <67081424+Mykytaxd@users.noreply.github.com>
2022-10-31 15:50:23 +0000

    Update 1

--- blockchain.py
+++ blockchain.py
@@ -2,25 +2,76 @@ blockchain = []
 
 
 def get_last_block_value():
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1.00]):
+def add_transaction(transaction_amount, last_transaction=[1]):
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
-    return float(input('Enter transaction amount'))
+def get_transaction_value():
+    user_input = float(input('Your transaction amount: '))
+    return user_input
 
 
-tx_amount = get_user_input()
-add_value(tx_amount)
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
 
-tx_amount = float(input('Enter transaction amount'))
-add_value(transaction_amount=tx_amount,
-          last_transaction=get_last_block_value())
 
-tx_amount = float(input('Enter transaction amount'))
-add_value(tx_amount, get_last_block_value())
+def print_block():
+    for block in blockchain:
+        print('Outputting block ')
+        print(block)
+    else:
+        print('-' * 20)
 
-print(blockchain)
+
+def verify_chain():
+    block_index = 0
+    is_valid = True
+    for block in blockchain:
+        if block_index == 0:
+            block_index += 1
+            continue
+        elif block[0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+        block_index += 1
+    return is_valid
+
+
+waiting_for_input = True
+
+
+while waiting_for_input:
+    print('Please choose ')
+    print('1: Add a new transaction ')
+    print('2: Output the blocks ')
+    print('3: Manipulate chain  ')
+    print('4: Quit  ')
+    user_choice = get_user_choice()
+
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_transaction(tx_amount, get_last_block_value())
+    elif user_choice == '2':
+        print_block()
+    elif user_choice == '3':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
+    elif user_choice == '4':
+        waiting_for_input = False
+    else:
+        print('Invalid input')
+    if not verify_chain():
+        print('Invalid block')
+        break
+else: 
+    print('User left')
